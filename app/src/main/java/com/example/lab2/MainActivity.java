974f269e0e84ee56a655edecb955fb96f1be7307
rkx24x <rehamalharbiksu@gmail.com>
2023-09-04 21:11:52 +0300

    hello

---
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button send_button;
+    EditText send_text;
+    TextView receiver_msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        send_button= (Button) findViewById(R.id.send_button_id);
+        send_text = (EditText) findViewById(R.id.send_text_id);
+        receiver_msg = (TextView) findViewById(R.id.received_value_id);
+
+        send_button.setOnClickListener(this::onClick);
+
+    }
+
+    private void onClick(View view){
+        String str = send_text.getText().toString();
+        receiver_msg.setText(" Hello, " +str);
+    };
+
+
+}
